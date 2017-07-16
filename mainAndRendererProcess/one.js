ef9fd45b5da8e63b66eac4e3963ac477a7cc45d9
Vishwas <gopinav5@gmail.com>
2017-07-16 15:32:11 +0530

    browserWindow from renderer process

--- mainAndRendererProcess/one.js
+++ mainAndRendererProcess/one.js
@@ -1,1 +1,16 @@
 console.log('renderer process 1');
+
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const path = require('path')
+const url = require('url')
+
+const newWindowBtn = document.getElementById('newWindowBtn');
+newWindowBtn.addEventListener('click', function (event) {
+let winThree = new BrowserWindow({ width: 600, height: 400 });
+winThree.loadURL(url.format({
+    pathname: path.join(__dirname, 'three.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+winThree.webContents.openDevTools();
+});
---
+++ mainAndRendererProcess/three.js
@@ -0,0 +1,1 @@
+console.log('Render process 3');
